7f87c1584bbeed1fdab1752caf38bf7ec9938c88
gsaini <gewrgia.saini@gmail.com>
2021-12-20 21:18:18 +0200

    pipes georgia

--- kernel_pipe.c
+++ kernel_pipe.c
@@ -1,9 +1,175 @@
-
+#include "kernel_pipe.h"
 #include "tinyos.h"
+#include "kernel_streams.h"
+#include "kernel_cc.h"
+#include "kernel_sched.h"
 
+Pipe_CB* pipe_init() {
+
+    Pipe_CB* new_pipe_cb = xmalloc(sizeof(Pipe_CB)); /* Space allocation of the new pipe control block */    
+    
+	/* Reader, Writer FCB's */
+    new_pipe_cb->reader = NULL;
+    new_pipe_cb->writer = NULL;
+
+    /* Reader and writer position of the buffer */
+    new_pipe_cb->w_position = 0;
+    new_pipe_cb->r_position = 0;
+
+    /* Condition variables initialized */
+    new_pipe_cb->has_space = COND_INIT;
+    new_pipe_cb->has_data = COND_INIT;
+
+    /* Current word length */
+    new_pipe_cb->write_data = 0;
+	new_pipe_cb->read_data = 0;
+
+    return new_pipe_cb;
+}
 
 int sys_Pipe(pipe_t* pipe)
 {
-	return -1;
+	Fid_t fid[2];
+    FCB* fcb[2];
+
+	int status = FCB_reserve(2,&fcb[0],&fid[0]);
+
+	if(status == 1){
+		Pipe_CB* new_pipe_cb = pipe_init();
+
+		/* Return read and write fid */
+		pipe_cb->read = fid[0]; 
+		pipe_cb->write = fid[1];
+
+		/* Set streams to point to the pipe_cb objects */
+		fcb[0]->streamobj = new_pipe_cb;
+    	fcb[1]->streamobj = new_pipe_cb;
+
+		/* Set the functions for read/write */
+    	fcb[0]->streamfunc = &readOperations ;
+    	fcb[1]->streamfunc = &writeOperations;
+	}
+	return 0;
 }
+
+int pipe_write(void* pipecb_t, const char *buf, unsigned int n){
+	PipeCB* pipe_cb = (PipeCB*)pipecb_t;
+
+	if(pipe_cb == NULL || pipe_cb->reader == NULL)
+		return -1;
+
+	int buffer_counter=0;
+	int write_data;
+
+	for(int i=0; i<n; i++){
+		pipe_cb->buffer[pipe_cb->w_position] = buf[i]; /* Store byte from our given buffer to the pipe_CB buffer */
+		pipe_cb->w_position++; 
+		pipe_cb->write_data++;
+		//piphs
+
+	    /* If writer wrote all buffer and reader is sleeping
+        until new data is read from the pipe, to free space */
+        while(pipe_cb->write_data == (int)PIPE_BUFFER_SIZE)
+            kernel_wait(&pipe_CB->has_space,SCHED_PIPE);
+
+        /* When writer pointer reaches end of buffer, cycle to the beginning */
+        if(pipe_cb->w_position == ((int)PIPE_BUFFER_SIZE - 1)){
+            pipe_cb->w_position = 0;
+        }
+
+		kernel_broadcast(&pipe_cb->has_data);
+	}
+  		return buffer_counter;
+	
+}
+
+int pipe_read(void* pipecb_t, char *buf, unsigned int n){
+	
+	PipeCB* pipe_cb = (PipeCB*)pipecb_t;
+	if(pipe_cb == NULL || pipe_cb->writer == NULL)
+		return -1;
+
+	int buffer_counter=0;
+	int read_data;
+
+	while(buffer_counter < n){ /* No data to Read */
+
+        while(pipe_CB->write_data==0){
+            if(pipe_CB->writer == NULL)
+                /*  In case there is no more data stored and writer is closed,
+                    return how much data has already been read.
+                    If writer was already closed when pipe_read() was called
+                    then it will return 0.
+                */
+                return buffer_counter;
+            /* if we expect someone to write, sleep till then*/
+            kernel_wait(&pipe_CB->has_data, SCHED_PIPE);
+        }
+
+	for (int i = 0; i < buffer_counter; i++){
+		buf[i]=pipe_cb->buffer[pipe_cb->r_position]; 
+		pipe_cb->r_position++;
+		pipe_cb->read_data--;
+		//pipa
+
+		/* When reader pointer reaches end of buffer, cycle to the beginning (Bounded Buffer) */
+        if(pipe_CB->r_position == ((int)PIPE_BUFFER_SIZE - 1)){
+            pipe_CB->r_position = 0;
+        }
+
+		if(pipe_cb->read_data<PIPE_BUFFER_SIZE)
+		kernel_broadcast(&pipe->has_space);
+		
+		buffer_counter++;
+	}
+  	return buffer_counter;
+
+}
+
+int pipe_writer_close(void* pipecb_t){
+	PipeCB* pipe_cb = (PipeCB*)pipecb_t;
+
+	if(pipe_cb == NULL)
+		return -1;
+
+	pipe_cb->writer = NULL
+
+	kernel_broadcast(&pipe_cb->has_data); /* Wake up the reader */ 
+
+	if(pipe_cb->reader == NULL)
+		free(pipe_cb);
+
+	return 0;
+}
+
+int pipe_reader_close(void* _pipecb){
+	PipeCB* pipe_cb = (PipeCB*)pipecb_t;
+
+	if(pipe_cb == NULL)
+		return -1;
+
+	pipe_cb->reader = NULL
+
+	kernel_broadcast(&pipe_cb->has_space); /* Wake up the writer */ 
+
+	if(pipe_cb->writer == NULL)
+		free(pipe_cb);
+
+	return 0;
+
+}
+
+static file_ops writeOperations = {
+  //.Open = serial_open,
+  .Read = NULL,
+  .Write = pipe_write,
+  .Close = pipe_writer_close
+};
+
+static file_ops readOperations = {
+  //.Open = serial_open,
+  .Read = pipe_read,
+  .Write = NULL,
+  .Close = pipe_reader_close
+};
 
---
+++ kernel_pipe.h
@@ -0,0 +1,41 @@
+#ifndef __KERNEL_PIPE_H
+#define __KERNEL_PIPE_H
+#include "tinyos.h"
+#include "kernel_streams.h"
+
+#define PIPE_BUFFER_SIZE 4096   /* Size of Buffer */
+
+/**
+  @brief Pipe Control Block.
+  This structure holds all information pertaining to a Pipe.
+ */
+
+typedef struct pipe_control_block {
+
+    FCB *reader, *writer; /* Pointers to read/write from buffer*/
+
+    CondVar has_space; /* For blocking writer if no space is available*/
+ 
+    CondVar has_data; /* For blocking reader until data are available*/ 
+
+    int w_position, r_position; /* Write and Read position in buffer*/
+
+    char buffer[PIPE_BUFFER_SIZE]; /* Bounded (cyclic) byte buffer*/
+
+    int buffer_counter,write_data,read_data;
+    
+} Pipe_CB;
+
+Pipe_CB* pipe_init();
+
+int sys_Pipe(pipe_t* pipe);
+
+int pipe_write(void* pipecb_t, const char *buf, unsigned int n);
+
+int pipe_read(void* pipecb_t, char *buf, unsigned int n);
+
+int pipe_writer_close(void* _pipecb);
+
+int pipe_reader_close(void* _pipecb);
+
+#endif
